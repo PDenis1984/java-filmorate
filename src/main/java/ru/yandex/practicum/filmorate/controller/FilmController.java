f877b377d1ed044d98230e217d106d61bc5a51c4
DenisVP <d.v.podolsky@mail.ru>
2025-11-19 17:13:21 +0300

    Change port to 8080, remove api/v1 from controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/films")
+@RequestMapping("/films")
 public class FilmController implements CrudInterface<Film> {
 
     private final FilmService filmService;
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 public class UserController implements CrudInterface<User> {
 
     private final UserService userService;
